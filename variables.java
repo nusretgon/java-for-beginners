1b1913b5679af6cd592b3d5e903542399471fa3d
Nusret GON <72438433+nusretgon@users.noreply.github.com>
2022-05-14 21:59:44 +0300

    Add files via upload

---
+++ variables.java
@@ -0,0 +1,32 @@
+public class variables {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a;
+		a=15;		// If we want we can initialize later.
+		
+		char c=65;				// ASCII number
+		char d='N';			
+		int age=15;
+		double number1=3.5;
+		float number2=5;		// We get 5.0
+		String name="Nusret";
+		boolean isFalse=true;
+		
+		double myAge=age;		// convert integer to double
+		
+		System.out.println("My name is "+name+".I am "+age+" years old.");
+		
+		System.out.println(number1+age);       // We get float number.
+		
+		System.out.println(name+age);          // We get string value. Nusret15
+		
+		System.out.println(c+age);             // 80
+		
+		System.out.println(isFalse);
+		
+		System.out.println(myAge);
+		
+		
+	}
+}
