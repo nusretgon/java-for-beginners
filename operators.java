b40de53577e696d10e26784d7b46b011292b2e0e
Nusret GON <72438433+nusretgon@users.noreply.github.com>
2022-05-14 21:59:58 +0300

    Add files via upload

---
+++ operators.java
@@ -0,0 +1,24 @@
+
+public class operators {
+
+	public static void main(String[] args) {
+		int num1=20,num2=30,num3=4,num4=6;
+		
+		System.out.println(num1+num2);
+		System.out.println(num1*num2);
+		System.out.println(num1/num2);
+		System.out.println(num1-num3);
+		System.out.println(num1%num4);		// Mod operator
+		
+		
+		System.out.println(num1--);			// Decrement
+		
+		System.out.println(num2++);			// Increment
+		
+		num3*=10;							// num3=num3*10
+		
+		System.out.println(num1==20);       // gives us is num1 equal 20 or else
+
+	}
+
+}
